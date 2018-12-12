87b714ec81353040b0fb8f68df42da24072fb5ff
Viktor Soroka <montanaqw5809@gmail.com>
2018-12-13 01:47:32 +0200

    Day 8: Memory Maneuver: Solve the second part

--- 2018/8/index.js
+++ 2018/8/index.js
@@ -6,38 +6,57 @@ const licenseData = parseFile(path.join(__dirname, 'data.txt'))[0]
   .split(' ')
   .map(Number);
 
-let totalAmount = 0;
-const rootNode = {
-  parent: null,
-  children: [],
-};
+let totalLicenseSum = 0;
+let rootLicenseNodeSum = 0;
+let rootNode = null;
 
-function processLicenseMetadata(parent, startIndex = 0) {
+const calculateSum = data => data.reduce((a, b) => a + b, 0);
+
+function countTotalLicenseMetadataSum(parent, startIndex = 0) {
   const amountOfNodes = licenseData[startIndex];
   const amountOfMetadata = licenseData[startIndex + 1];
 
   const node = {
-    amountOfNodes,
     metadata: [],
     children: [],
   };
 
-  parent.children.push(node);
+  if (parent) {
+    parent.children.push(node);
+  } else {
+    rootNode = node;
+  }
 
   let endIndex = startIndex + 2;
 
   for (let i = 0; i < amountOfNodes; i++) {
-    endIndex = processLicenseMetadata(node, endIndex);
+    endIndex = countTotalLicenseMetadataSum(node, endIndex);
   }
 
   const metadata = licenseData.slice(endIndex, endIndex + amountOfMetadata);
 
   node.metadata.push(...metadata);
-  totalAmount += metadata.reduce((a, b) => a + b, 0);
+  totalLicenseSum += calculateSum(metadata);
 
   return endIndex + amountOfMetadata;
 }
 
-processLicenseMetadata(rootNode);
+function countRootLicenseNodeMetadataSum(node) {
+  node.metadata.forEach((meta) => {
+    const child = node.children[meta - 1];
 
-console.log('Part 1:', totalAmount);
+    if (child) {
+      if (!child.children.length) {
+        rootLicenseNodeSum += calculateSum(child.metadata);
+      } else {
+        countRootLicenseNodeMetadataSum(child);
+      }
+    }
+  });
+}
+
+countTotalLicenseMetadataSum(rootNode);
+countRootLicenseNodeMetadataSum(rootNode);
+
+console.log('Part 1:', totalLicenseSum);
+console.log('Part 2:', rootLicenseNodeSum);
